33ec504c4ef0862ffdfbb51a4f87eee1c3be39e9
DevDJ <djkondzio@onet.pl>
2021-10-25 20:24:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+﻿<?
+if(!isset($_COOKIE['waf-redict'])&&$_COOKIE['waf-redict']=="false"){
+	include_once("waf/index.php");
+	setcookie('waf-redict', 'false');
+	$_COOKIE['waf-redict'] = "false";
+}else if($_COOKIE['waf-redict']=="true"){
+	include_once("include/index.html");
+}else{
+	include_once("waf/index.php");
+}
+
+?>
